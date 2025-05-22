204f318546ca999e4dce2076e9ffbb58b3720b6f
naka-toshiki <hp231068@senshu-u.jp>
2025-05-22 18:04:29 +0900

    aaaa

--- 中俊貴HP231-68H.R
+++ 中俊貴HP231-68H.R
@@ -52,16 +52,65 @@ hoge3
 library(tidyverse)
 hoge3<-as_tibble(hoge3)
 hoge3
+##ベースボール
+library("tidyverse")
+dat<-read.csv("BaseballDecade.csv")
+head(dat)
+tail(dat)
+dim(dat)
+names(dat)
+colnames(dat)
+summary(dat)
+str(dat)
+class(dat)
+dat.tb<-dat
+dat.tb
+
+dat.tb$Name
+table(dat.tb$Name)
+dat.tb$Name %>% table() %>% sort(decreasing = TRUE)
+dat.tb$team %>% unique()
+dat.tb$team %>% unique() %>% length()
+dat.tb$team <-dat.tb$team %>% as.factor()
+dat.tb$bloodType <-dat.tb$bloodType<-dat.tb$bloodType %>% as.factor()
+dat.tb$bloodType
+dat.tb$position<-dat.tb$position %>% as.factor()
+dat.tb %>% select(team,bloodType,position) %>% summary()
+dat.tb$height %>% mean()
+dat.tb$height %>% var()
+dat.tb$salary %>% quantile()
+dat.tb$salary %>% quantile(probs=0,0.25,0.33,0.95,1)
+dat.tb<-dat.tb %>% mutate(bmi_category=ifelse(bim>=25,"HighBMI","Standard"))
+install.packages("dplyr")
+library("dplyr")
+dat.tb <- dat.tb %>% mutate(bmi = weight / ((height/100)^2)) 
+dat.tb$bmi_category %>% table()
+
+dat.tb <- dat.tb %>%
+  mutate(position2 = case_when(position == "投手" ~ "投手", TRUE ~ "野手"))
+  
+  
+
+# dat.tb を data.frame に変換
+dat.tb <- as.data.frame(dat.tb)
+
+# その後、mutate 関数を適用
+dat.tb <- dat.tb %>% mutate(bmi = weight / ((height/100)^2))
 
 
+dat.tb <- as_tibble(dat.tb)
 
+# その後、mutate 関数を適用
+dat.tb <- dat.tb %>% mutate(bmi = weight / ((height/100)^2))
 
-
-
-
-
-
-
-
-
-
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
